b99a14d4cd1c805f652917e5a1d572b91b587157
Muin <abid.muin@reddotdigitalit.com>
2021-07-20 13:31:29 +0600

    completed "Contact Me" section

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,18 +22,35 @@ if (navClose) {
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll(`[data-target]`),
+    tabContents = document.querySelectorAll(`[data-content]`)
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
